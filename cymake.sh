1a7740080cd866600ee17946ca348dc239b889f0
Sheikh <sksaad48@gmail.com>
2021-02-28 22:15:04 +0600

    update

--- cymake.sh
+++ cymake.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # compile a single-file cython code into native binary
 
-# check for residue files and remove them
-[ test -f "./$1" ] && rm "./$1"
-[ test -f "./$1.c" ] && rm "./$1.c"
+# check for residue files and remove them (doesn't work yet. I'll figure it out...)
+# [ test -e "./$1" ] && rm "./$1"
+# [ test -e "./$1.c" ] && rm "./$1.c"
 
 # check format of existing source file
-[ test -f "./$1.pyx" ] && srcname="$1.pyx" || [ test -f "./$1.py" ] && srcname="$1.py" || echo "No source file with name '$1' found in current directory." && exit 1
+# [ test -e "./$1.pyx" ] && srcname="$1.pyx" || [ test -e "./$1.py" ] && srcname="$1.py" || echo "No source file with name '$1' found in current directory." && exit 1
+
+srcname="$1.pyx"
 
 # check the installed Python 3 version and format it for use
 pyver=$(python3 -V | cut -d "." -f 1-2 | tr '[:upper:]' '[:lower:]' | sed 's/ //g')
